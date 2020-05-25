dd4bddea9a016bcbaba01dcfb62a4e5cd1b725de
Dr. ERDI Gergo <gergo@erdi.hu>
2020-05-25 21:57:34 +0800

    Move video signal generator to a separate function

--- src/FrameBuffer.hs
+++ src/FrameBuffer.hs
@@ -19,19 +19,27 @@ topEntity
     -> "VGA" ::: VGAOut Dom25 8 8 8
 topEntity = withEnableGen board
   where
-    board = vgaOut vgaSync rgb
+    board = vga
       where
-        VGADriver{..} = vgaDriver vga640x480at60
-        rgb = monochrome <$> current
-
-        frameEnd = isFalling False (isJust <$> vgaY)
-        vgaXY = bundle (fromMaybe 0 <$> vgaX, fromMaybe 0 <$> vgaY)
+        (frameEnd, vga) = video write
 
         ptr = regEn (0, 0) frameEnd $ nextPtr <$> ptr
-
-        addr = fbAddress <$> vgaXY
         write = packWrite <$> (fbAddress <$> ptr) <*> pure (Just 1)
-        current = blockRam1 ClearOnReset (SNat @FBSize) 0 addr write
+
+video
+    :: (HiddenClockResetEnable Dom25)
+    => Signal Dom25 (Maybe (Unsigned FBWidth, Bit))
+    -> (Signal Dom25 Bool, VGAOut Dom25 8 8 8)
+video write = (frameEnd, vgaOut vgaSync rgb)
+  where
+    VGADriver{..} = vgaDriver vga640x480at60
+    rgb = monochrome <$> current
+
+    frameEnd = isFalling False (isJust <$> vgaY)
+    vgaXY = bundle (fromMaybe 0 <$> vgaX, fromMaybe 0 <$> vgaY)
+
+    addr = fbAddress <$> vgaXY
+    current = blockRam1 ClearOnReset (SNat @FBSize) 0 addr write
 
 type FBWidth = CLog 2 480 + CLog 2 640
 type FBSize = 2 ^ FBWidth
