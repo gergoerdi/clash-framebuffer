1a25e4603db2a6f1c0426f4176fbb59ab70d07cf
Dr. ERDI Gergo <gergo@erdi.hu>
2020-06-06 10:56:03 +0800

    Linear addressing

--- src/FrameBuffer.hs
+++ src/FrameBuffer.hs
@@ -17,18 +17,16 @@ topEntity
     :: "CLK_25MHZ" ::: Clock Dom25
     -> "RESET" ::: Reset Dom25
     -> "VGA" ::: VGAOut Dom25 8 8 8
-topEntity = withEnableGen board
+topEntity = withEnableGen vga
   where
-    board = vga
-      where
-        (frameEnd, vga) = video write
+    (frameEnd, vga) = video write
 
-        ptr = regEn (0, 0) frameEnd $ nextPtr <$> ptr
-        write = packWrite <$> (fbAddress <$> ptr) <*> pure (Just 1)
+    ptr = regEn 0 frameEnd $ ptr + 1
+    write = packWrite <$> ptr <*> pure (Just 1)
 
 video
     :: (HiddenClockResetEnable Dom25)
-    => Signal Dom25 (Maybe (Unsigned FBWidth, Bit))
+    => Signal Dom25 (Maybe (Index (640 * 480), Bit))
     -> (Signal Dom25 Bool, VGAOut Dom25 8 8 8)
 video write = (frameEnd, vgaOut vgaSync rgb)
   where
@@ -36,21 +34,12 @@ video write = (frameEnd, vgaOut vgaSync rgb)
     rgb = monochrome <$> current
 
     frameEnd = isFalling False (isJust <$> vgaY)
-    vgaXY = bundle (fromMaybe 0 <$> vgaX, fromMaybe 0 <$> vgaY)
+    lineEnd = isFalling False (isJust <$> vgaX)
 
-    addr = fbAddress <$> vgaXY
-    current = blockRam1 ClearOnReset (SNat @FBSize) 0 addr write
+    base = regEn 0 lineEnd $ mux (isNothing <$> vgaY) 0 (base + 640)
+    address = base + (maybe 0 fromIntegral <$> vgaX)
 
-type FBWidth = CLog 2 480 + CLog 2 640
-type FBSize = 2 ^ FBWidth
-
-fbAddress :: (Index 640, Index 480) -> Unsigned FBWidth
-fbAddress (x, y) = bitCoerce (y, x)
-
-nextPtr :: (Index 640, Index 480) -> (Index 640, Index 480)
-nextPtr (x, y) = case succIdx x of
-    Just x' -> (x', y)
-    Nothing -> (0, nextIdx y)
+    current = blockRam1 ClearOnReset (SNat @(640 * 480)) 0 address write
 
 monochrome :: (Bounded a) => Bit -> a
 monochrome 0 = minBound
