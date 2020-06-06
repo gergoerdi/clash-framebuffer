46ac75af9dd9d7f1abc35bf48af41a9e314ffcc4
Dr. ERDI Gergo <gergo@erdi.hu>
2020-06-06 23:21:53 +0800

    Start using `DSignal`s

--- src/FrameBuffer.hs
+++ src/FrameBuffer.hs
@@ -31,10 +31,10 @@ frameBuffer
     -> Signal dom (Maybe (Index (w * h), a))
     -> Signal dom (Maybe (Index w))
     -> Signal dom (Maybe (Index h))
-    -> Signal dom (Maybe a)
-frameBuffer initial write x y = enable (delay False visible) current
+    -> DSignal dom 1 (Maybe a)
+frameBuffer initial write x y = enable (delayedI False visible) current
   where
-    visible = isJust <$> x .&&. isJust <$> y
+    visible = fromSignal $ isJust <$> x .&&. isJust <$> y
 
     prevY = register Nothing y
     newLine = y ./=. prevY
@@ -48,13 +48,13 @@ frameBuffer initial write x y = enable (delay False visible) current
 
     address = base' + (maybe 0 fromIntegral <$> x)
 
-    current = blockRam1 ClearOnReset (SNat @(w * h)) initial address write
+    current = delayedBlockRam1 ClearOnReset (SNat @(w * h)) initial (fromSignal address) (fromSignal write)
 
 video
     :: (HiddenClockResetEnable Dom25)
     => Signal Dom25 (Maybe (Index (200 * 140), Bit))
     -> (Signal Dom25 Bool, VGAOut Dom25 8 8 8)
-video write = (frameEnd, vgaOut (delaySync vgaSync) rgb)
+video write = (frameEnd, delayVGA vgaSync rgb)
   where
     VGADriver{..} = vgaDriver vga640x480at60
     frameEnd = isFalling False (isJust <$> vgaY)
@@ -64,7 +64,7 @@ video write = (frameEnd, vgaOut (delaySync vgaSync) rgb)
 
     rgb = maybe <$> grid <*> pure monochrome <*> frameBuffer 0 write vgaX' vgaY'
 
-    grid = mux parity red green
+    grid = toDelayedU $ mux parity red green
     parity = (maybe 0 lsb <$> vgaX) .==. (maybe 0 lsb <$> vgaY)
     red = pure (255, 0, 0)
     green = pure (0, 255, 0)
@@ -75,6 +75,50 @@ delaySync VGASync{..} = VGASync
     , vgaVSync = register undefined vgaVSync
     , vgaDE = register False vgaDE
     }
+
+delayVGA
+    :: (KnownNat d, KnownNat r, KnownNat g, KnownNat b)
+    => (HiddenClockResetEnable dom)
+    => VGASync dom
+    -> DSignal dom d (Unsigned r, Unsigned g, Unsigned b)
+    -> VGAOut dom r g b
+delayVGA VGASync{..} rgb = vgaOut vgaSync' (toSignal rgb)
+  where
+    vgaSync' = VGASync
+        { vgaHSync = matchDelay rgb vgaHSync
+        , vgaVSync = matchDelay rgb vgaVSync
+        , vgaDE = matchDelay rgb vgaDE
+        }
+
+matchDelay
+    :: (KnownNat d, NFDataX a, HiddenClockResetEnable dom)
+    => DSignal dom d any
+    -> Signal dom a
+    -> Signal dom a
+matchDelay d = toSignal . (d *>) . toDelayedU
+
+delayedU
+  :: (KnownNat d, NFDataX a, HiddenClockResetEnable dom)
+  => DSignal dom n a
+  -> DSignal dom (n + d) a
+delayedU = delayedI undefined
+
+toDelayedU
+  :: (KnownNat d, NFDataX a, HiddenClockResetEnable dom)
+  => Signal dom a
+  -> DSignal dom d a
+toDelayedU = delayedU . fromSignal
+
+delayedBlockRam1
+    :: (1 <= m, Enum addr, NFDataX a, HiddenClockResetEnable dom)
+    => ResetStrategy r
+    -> SNat m
+    -> a
+    -> DSignal dom d addr
+    -> DSignal dom d (Maybe (addr, a))
+    -> DSignal dom (d + 1) a
+delayedBlockRam1 resetStrat size content addr wr = unsafeFromSignal $
+    blockRam1 resetStrat size content (toSignal addr) (toSignal wr)
 
 monochrome :: (Bounded a) => Bit -> a
 monochrome 0 = minBound
